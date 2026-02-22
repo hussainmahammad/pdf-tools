27653418c1037bf46dd270f614c1740925366bff
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 13:08:26 +0530

    Update lambda.tf

--- files/terraform/lambda.tf
+++ files/terraform/lambda.tf
@@ -1,11 +1,13 @@
-# Upload function
+/* ================= UPLOAD FUNCTION ================= */
+
 resource "aws_lambda_function" "upload" {
-  function_name = "${var.project}-upload-function"
+  function_name = "${local.prefix}-upload"
   role          = aws_iam_role.lambda_role.arn
   runtime       = "python3.11"
   handler       = "lambda_function.lambda_handler"
 
-  filename = "../../app/functions/upload.zip"
+  filename         = "../../app/functions/upload.zip"
+  source_code_hash = filebase64sha256("../../app/functions/upload.zip")
 
   environment {
     variables = {
@@ -14,16 +16,21 @@ resource "aws_lambda_function" "upload" {
   }
 }
 
-# Process function
+
+/* ================= PROCESS FUNCTION ================= */
+
 resource "aws_lambda_function" "process" {
-  function_name = "${var.project}-process-function"
+  function_name = "${local.prefix}-process"
   role          = aws_iam_role.lambda_role.arn
   runtime       = "python3.11"
   handler       = "lambda_function.lambda_handler"
 
-  filename = "../../app/functions/process.zip"
+  filename         = "../../app/functions/process.zip"
+  source_code_hash = filebase64sha256("../../app/functions/process.zip")
 
-  layers = [aws_lambda_layer_version.pdf_layer.arn]
+  layers = [
+    aws_lambda_layer_version.pdf_layer.arn
+  ]
 
   environment {
     variables = {
