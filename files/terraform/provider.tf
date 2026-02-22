26f5ebf1bca8e5c87c5af6fca672b8a99b89a101
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 13:28:28 +0530

    Update provider.tf

--- files/terraform/provider.tf
+++ files/terraform/provider.tf
@@ -5,6 +5,15 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "petcart-terraform-state-867809929056"
+    key            = "pdf-tools/prod.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    # Uncomment if your other projects use state locking
+    # dynamodb_table = "terraform-locks"
+  }
 }
 
 provider "aws" {
