bb20982802b903e010662984c8d10c7da5d5f754
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 12:31:28 +0530

    Update lambda.tf

--- files/terraform/lambda.tf
+++ files/terraform/lambda.tf
@@ -1,1 +1,33 @@
+# Upload function
+resource "aws_lambda_function" "upload" {
+  function_name = "${var.project}-upload-function"
+  role          = aws_iam_role.lambda_role.arn
+  runtime       = "python3.11"
+  handler       = "lambda_function.lambda_handler"
 
+  filename = "../../app/functions/upload.zip"
+
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.files.bucket
+    }
+  }
+}
+
+# Process function
+resource "aws_lambda_function" "process" {
+  function_name = "${var.project}-process-function"
+  role          = aws_iam_role.lambda_role.arn
+  runtime       = "python3.11"
+  handler       = "lambda_function.lambda_handler"
+
+  filename = "../../app/functions/process.zip"
+
+  layers = [aws_lambda_layer_version.pdf_layer.arn]
+
+  environment {
+    variables = {
+      BUCKET_NAME = aws_s3_bucket.files.bucket
+    }
+  }
+}
