42750b8fcf04cc5997cb38a2a327b2f2f15f6d8b
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 12:31:50 +0530

    Update apigateway.tf

--- files/terraform/apigateway.tf
+++ files/terraform/apigateway.tf
@@ -1,1 +1,54 @@
+resource "aws_apigatewayv2_api" "api" {
+  name          = "${var.project}-api"
+  protocol_type = "HTTP"
 
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["*"]
+    allow_headers = ["*"]
+  }
+}
+
+resource "aws_apigatewayv2_integration" "upload" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.upload.invoke_arn
+}
+
+resource "aws_apigatewayv2_integration" "process" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.process.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "upload" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /upload"
+  target    = "integrations/${aws_apigatewayv2_integration.upload.id}"
+}
+
+resource "aws_apigatewayv2_route" "process" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /process"
+  target    = "integrations/${aws_apigatewayv2_integration.process.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_upload" {
+  statement_id  = "AllowAPIGatewayUpload"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.upload.function_name
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_lambda_permission" "api_process" {
+  statement_id  = "AllowAPIGatewayProcess"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.process.function_name
+  principal     = "apigateway.amazonaws.com"
+}
