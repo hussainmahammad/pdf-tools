801228c6d4a82222a2b3541b4a6eea5f6d23b59b
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 13:10:44 +0530

    Update apigateway.tf

--- files/terraform/apigateway.tf
+++ files/terraform/apigateway.tf
@@ -1,25 +1,35 @@
+/* ================= API ================= */
+
 resource "aws_apigatewayv2_api" "api" {
-  name          = "${var.project}-api"
+  name          = "${local.prefix}-api"
   protocol_type = "HTTP"
 
   cors_configuration {
     allow_origins = ["*"]
-    allow_methods = ["*"]
+    allow_methods = ["POST", "OPTIONS"]
     allow_headers = ["*"]
   }
 }
 
+
+/* ================= INTEGRATIONS ================= */
+
 resource "aws_apigatewayv2_integration" "upload" {
-  api_id           = aws_apigatewayv2_api.api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.upload.invoke_arn
+  api_id                 = aws_apigatewayv2_api.api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.upload.invoke_arn
+  payload_format_version = "2.0"
 }
 
 resource "aws_apigatewayv2_integration" "process" {
-  api_id           = aws_apigatewayv2_api.api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.process.invoke_arn
+  api_id                 = aws_apigatewayv2_api.api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.process.invoke_arn
+  payload_format_version = "2.0"
 }
+
+
+/* ================= ROUTES ================= */
 
 resource "aws_apigatewayv2_route" "upload" {
   api_id    = aws_apigatewayv2_api.api.id
@@ -33,22 +43,30 @@ resource "aws_apigatewayv2_route" "process" {
   target    = "integrations/${aws_apigatewayv2_integration.process.id}"
 }
 
+
+/* ================= STAGE ================= */
+
 resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
 }
 
+
+/* ================= LAMBDA PERMISSIONS ================= */
+
 resource "aws_lambda_permission" "api_upload" {
-  statement_id  = "AllowAPIGatewayUpload"
+  statement_id  = "${local.prefix}-AllowUpload"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.upload.function_name
   principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
 
 resource "aws_lambda_permission" "api_process" {
-  statement_id  = "AllowAPIGatewayProcess"
+  statement_id  = "${local.prefix}-AllowProcess"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.process.function_name
   principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
