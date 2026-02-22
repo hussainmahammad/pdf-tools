e1db18dfcb6279b01335eedf273771a5c7b424da
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 12:31:01 +0530

    Update layer.tf

--- files/terraform/layer.tf
+++ files/terraform/layer.tf
@@ -1,1 +1,5 @@
-
+resource "aws_lambda_layer_version" "pdf_layer" {
+  filename            = "../../app/layers/layer.zip"
+  layer_name          = "${var.project}-layer"
+  compatible_runtimes = ["python3.11"]
+}
