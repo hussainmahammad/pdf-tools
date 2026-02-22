56e2e7ee8ef19749137cbfb79cb0d13b69549820
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 12:30:38 +0530

    Update s3.tf

--- files/terraform/s3.tf
+++ files/terraform/s3.tf
@@ -1,1 +1,39 @@
+resource "aws_s3_bucket" "ui" {
+  bucket = "${var.project}-${var.env}-ui"
+}
 
+resource "aws_s3_bucket_public_access_block" "ui" {
+  bucket = aws_s3_bucket.ui.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "ui" {
+  bucket = aws_s3_bucket.ui.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "ui_public" {
+  bucket = aws_s3_bucket.ui.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = "*"
+      Action = "s3:GetObject"
+      Resource = "${aws_s3_bucket.ui.arn}/*"
+    }]
+  })
+}
+
+# Processing bucket
+resource "aws_s3_bucket" "files" {
+  bucket = "${var.project}-${var.env}-files"
+}
