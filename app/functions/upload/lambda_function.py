57510035cf235075c1ca063e570450b50becc32c
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 12:06:27 +0530

    Update lambda_function.py

--- app/functions/upload/lambda_function.py
+++ app/functions/upload/lambda_function.py
@@ -1,1 +1,49 @@
+import boto3
+import json
+import uuid
+import os
 
+s3 = boto3.client('s3')
+BUCKET = os.environ['BUCKET_NAME']
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+}
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event.get('body', '{}'))
+        filename = body.get("filename")
+
+        # If filename provided → use it
+        if filename:
+            key = f"uploads/{filename}"
+        else:
+            key = f"uploads/{uuid.uuid4()}.pdf"
+
+        url = s3.generate_presigned_url(
+            'put_object',
+            Params={
+                'Bucket': BUCKET,
+                'Key': key,
+                'ContentType': 'application/pdf'
+            },
+            ExpiresIn=300
+        )
+
+        return {
+            "statusCode": 200,
+            "headers": HEADERS,
+            "body": json.dumps({
+                "upload_url": url,
+                "file_key": key
+            })
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "headers": HEADERS,
+            "body": json.dumps({"error": str(e)})
+        }
