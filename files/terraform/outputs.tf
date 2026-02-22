4ffe47a9dfa681f1c68a8d3fc547594d2fddc639
hussainmahammad <hussainmahammad.kattubadi@gmail.com>
2026-02-22 12:32:18 +0530

    Update outputs.tf

--- files/terraform/outputs.tf
+++ files/terraform/outputs.tf
@@ -1,1 +1,11 @@
+output "ui_bucket" {
+  value = aws_s3_bucket.ui.bucket
+}
 
+output "ui_url" {
+  value = aws_s3_bucket_website_configuration.ui.website_endpoint
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_api.api.api_endpoint
+}
